873856724ea0a362873bdd428571e502dca338c2
marcalph <alphonsus.marc@gmail.com>
2022-03-18 09:56:14 +0100

    sql

--- sql/534.sql
+++ sql/534.sql
@@ -14,7 +14,7 @@
 -- Write an SQL query to report for each player and date, how many games played so far by the player. That is, the total number of games played by the player until that date. Check the example for clarity.
 """
 SELECT player_id,event_date, SUM(games_played)
-OVER (PARTITION player_id ORDER BY event_date)
+OVER (PARTITION BY player_id ORDER BY event_date)
 FROM activity
 """
 
