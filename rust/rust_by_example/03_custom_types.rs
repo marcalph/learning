0a57715fb690a2d2704e007d57ffa29d6a9a4ca2
marcalph <marcalph@protonmail.com>
2022-11-05 14:49:06 +0100

    structs

--- rust/rust_by_example/03_custom_types.rs
+++ rust/rust_by_example/03_custom_types.rs
@@ -14,12 +14,14 @@ struct Unit;
 struct Pair(i32, f32);
 
 // A struct with two fields
+#[derive(Debug)]
 struct Point {
     x: f32,
     y: f32,
 }
 
 // Structs can be reused as fields of another struct
+#[derive(Debug)]
 struct Rectangle {
     // A rectangle can be specified by where the top left and bottom right
     // corners are in space.
@@ -55,11 +57,12 @@ fn main() {
     // Destructure the point using a `let` binding ???
     let Point { x: left_edge, y: top_edge } = point;
 
-    let _rectangle = Rectangle {
+    let mut _rectangle = Rectangle {
         // struct instantiation is an expression too
         top_left: Point { x: left_edge, y: top_edge },
         bottom_right: bottom_right,
     };
+    _rectangle.bottom_right.y = 6.;
 
     // Instantiate a unit struct
     let _unit = Unit;
@@ -74,4 +77,131 @@ fn main() {
     let Pair(integer, decimal) = pair;
 
     println!("destructured pair contains {:?} and {:?}", integer, decimal);
+
+    println!("{}", rect_area(_rectangle));
+    println!("{:?}", square(point, 5.));
+
+
+
+    let pressed = WebEvent::KeyPress('x');
+    // `to_owned()` creates an owned `String` from a string slice.
+    let pasted  = WebEvent::Paste("my text".to_owned());
+    let click   = WebEvent::Click { x: 20, y: 80 };
+    let load    = WebEvent::PageLoad;
+    let unload  = WebEvent::PageUnload;
+
+    inspect(pressed);
+    inspect(pasted);
+    inspect(click);
+    inspect(load);
+    inspect(unload);
+
+
+    let x= Operations::Add;
+    println!("{}", x.run(3, 5));
+
+
+    // use declaration
+    use crate::Status::{Poor, Rich};
+    // Automatically `use` each name inside `Work`.
+    use crate::Work::*;
+
+    // Equivalent to `Status::Poor`.
+    let status = Poor;
+    // Equivalent to `Work::Civilian`.
+    let work = Civilian;
+
+    match status {
+        // Note the lack of scoping because of the explicit `use` above.
+        Rich => println!("The rich have lots of money!"),
+        Poor => println!("The poor have no money..."),
+    }
+
+    match work {
+        // Note again the lack of scoping.
+        Civilian => println!("Civilians work!"),
+        Soldier  => println!("Soldiers fight!"),
+    }
+
 }
+
+
+// compute area
+fn rect_area(rect: Rectangle) -> f32{
+    let Rectangle{top_left: topl, bottom_right: botr} = rect;
+    let side0 = (topl.x - botr.x).abs();
+    let side1 = (topl.y - botr.y).abs();
+    println!("{topl:?} {botr:?}");
+    side0*side1
+}
+// create square
+fn square(point: Point, side: f32) -> Rectangle {
+    let botr =  Point{x:point.x+side, y:point.y+side};
+    Rectangle{top_left: point, bottom_right:botr}
+}
+
+// enums and type aliases
+enum WebEvent {
+    // An `enum` may either be `unit-like`,
+    PageLoad,
+    PageUnload,
+    // like tuple structs,
+    KeyPress(char),
+    Paste(String),
+    // or c-like structures.
+    Click { x: i64, y: i64 },
+}
+// A function which takes a `WebEvent` enum as an argument and
+// returns nothing.
+fn inspect(event: WebEvent) {
+    match event {
+        WebEvent::PageLoad => println!("page loaded"),
+        WebEvent::PageUnload => println!("page unloaded"),
+        // Destructure `c` from inside the `enum`.
+        WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
+        WebEvent::Paste(s) => println!("pasted \"{}\".", s),
+        // Destructure `Click` into `x` and `y`.
+        WebEvent::Click { x, y } => {
+            println!("clicked at x={}, y={}.", x, y);
+        },
+    }
+}
+
+
+enum VeryVerboseEnumOfThingsToDoWithNumbers {
+    Add,
+    Subtract,
+}
+
+// Creates a type alias
+type Operations = VeryVerboseEnumOfThingsToDoWithNumbers;
+
+
+impl Operations {
+    fn run(&self, x:i32, y: i32) -> i32 {
+        match self {
+            Self::Add => x+y,
+            Self::Subtract =>x-y
+        }
+    }
+}
+
+// implicit discriminator (0 .. n)
+enum Status {
+    Rich,
+    Poor,
+}
+
+// explicit discriminator
+enum Work {
+    Civilian=0xff,
+    Soldier=0x00,
+}
+
+
+// Globals are declared outside all other scopes.
+static LANGUAGE: &str = "Rust";
+const THRESHOLD: i32 = 10;
+
+// const: An unchangeable value (the common case).
+// static: A possibly mutable variable with 'static lifetime. The static lifetime is inferred and does not have to be specified. Accessing or modifying a mutable static variable is unsafe.
