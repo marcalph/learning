286651964c4545a0ab51eb7047931095d87ba416
marcalph <marcalph@protonmail.com>
2022-11-19 12:26:33 +0100

    rust funcs

--- rust/rust_by_example/09_functions.rs
+++ rust/rust_by_example/09_functions.rs
@@ -12,6 +12,8 @@ fn main() {
     output_parameters();
     iterator_any();
     iterator_find();
+    hof();
+    diverging();
 }
 
 // Function that returns a boolean value
@@ -476,3 +478,75 @@ fn iterator_find() {
 }
 
 
+fn is_odd(n: u32) -> bool {
+    n % 2 == 1
+}
+
+fn hof() {
+    println!("Find the sum of all the squared odd numbers under 1000");
+    let upper = 1000;
+
+    // Imperative approach
+    // Declare accumulator variable
+    let mut acc = 0;
+    // Iterate: 0, 1, 2, ... to infinity
+    for n in 0.. {
+        // Square the number
+        let n_squared = n * n;
+
+        if n_squared >= upper {
+            // Break loop if exceeded the upper limit
+            break;
+        } else if is_odd(n_squared) {
+            // Accumulate value, if it's odd
+            acc += n_squared;
+        }
+    }
+    println!("imperative style: {}", acc);
+
+    // Functional approach
+    let sum_of_squared_odd_numbers: u32 =
+        (0..).map(|n| n * n)                             // All natural numbers squared
+             .take_while(|&n_squared| n_squared < upper) // Below upper limit
+             .filter(|&n_squared| is_odd(n_squared))     // That are odd
+             .sum();                                     // Sum them
+    println!("functional style: {}", sum_of_squared_odd_numbers);
+}
+
+
+fn some_fn() {
+    ()
+}
+
+
+fn diverging() {
+    // fn not_empty() {
+    //     let a: () = some_fn();
+    //     println!("This function returns and you can see this line.")
+    // }
+    
+    // fn empty() {
+    //     let x: ! = panic!("This call never returns.");
+    //     println!("You will never see this line!");
+    // }
+    fn sum_odd_numbers(up_to: u32) -> u32 {
+        let mut acc = 0;
+        for i in 0..up_to {
+            // Notice that the return type of this match expression must be u32
+            // because of the type of the "addition" variable.
+            let addition: u32 = match i%2 == 1 {
+                // The "i" variable is of type u32, which is perfectly fine.
+                true => i,
+                // On the other hand, the "continue" expression does not return
+                // u32, but it is still fine, because it never returns and therefore
+                // does not violate the type requirements of the match expression.
+                // It is also the return type of functions that loop forever (e.g. loop {}) 
+                // like network servers or functions that terminate the process (e.g. exit()).
+                false => continue,
+            };
+            acc += addition;
+        }
+        acc
+    }
+    println!("Sum of odd numbers up to 9 (excluding): {}", sum_odd_numbers(9));
+}
