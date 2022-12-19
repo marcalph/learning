d9155b8926a2ab8e1be71fbffb77f0b3c98e7a0c
marcalph <marcalph@protonmail.com>
2022-12-19 23:18:48 +0100

    WIP

--- rust/rust_by_example/14_generics.rs
+++ rust/rust_by_example/14_generics.rs
@@ -1,6 +1,7 @@
 fn main() {
     generic_functions();
     generic_implementations();
+    generic_traits();
     
 }
 struct A;          // Concrete type `A`.
@@ -72,4 +73,34 @@ fn generic_implementations() {
 }
 
 
+// Non-copyable types.
+struct Empty;
+struct Null;
+
+// A trait generic over `T`.
+trait DoubleDrop<T> {
+    // Define a method on the caller type which takes an
+    // additional single parameter `T` and does nothing with it.
+    fn double_drop(self, _: T);
+}
+
+// Implement `DoubleDrop<T>` for any generic parameter `T` and
+// caller `U`.
+impl<T, U> DoubleDrop<T> for U {
+    // This method takes ownership of both passed arguments,
+    // deallocating both.
+    fn double_drop(self, _: T) {}
+}
+
+fn generic_traits() {
+    let empty = Empty;
+    let null  = Null;
+
+    // Deallocate `empty` and `null`.
+    empty.double_drop(null);
+
+    //empty;
+    //null;
+    // ^ TODO: Try uncommenting these lines.
+}
 
