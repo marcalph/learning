62859fd8a34bbd6fb90bcb1762ee499bac8295aa
marcalph <marcalph@protonmail.com>
2023-01-01 21:07:06 +0100

    :fire: scoping

---
+++ rust/rust_by_example/15_scoping_rules.rs
@@ -0,0 +1,32 @@
+fn main() {
+    // Allocate an integer on the heap
+    let _box2 = Box::new(5i32);
+    // A nested scope:
+    {
+        // Allocate an integer on the heap
+        let _box3 = Box::new(4i32);
+        // `_box3` is destroyed here, and memory gets freed
+    }
+    // Creating lots of boxes just for fun
+    // There's no need to manually free memory!
+    for _ in 0u32..1_000 {
+        raii();
+    }
+    // `_box2` is destroyed here, and memory gets freed
+    let _x = ToDrop;
+    println!("Made a ToDrop!");
+}
+
+fn raii() {
+    // Allocate an integer on the heap
+    let _box1 = Box::new(3i32);
+    // `_box1` is destroyed here, and memory gets freed
+}
+struct ToDrop;
+
+impl Drop for ToDrop {
+    fn drop(&mut self) {
+        println!("ToDrop is being dropped");
+    }
+}
+
