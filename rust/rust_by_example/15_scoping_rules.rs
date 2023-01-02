c1748759998e2c89bd4b2562b8244ba3dc035578
marcalph <marcalph@protonmail.com>
2023-01-02 17:10:55 +0100

    :fire: rust scope

--- rust/rust_by_example/15_scoping_rules.rs
+++ rust/rust_by_example/15_scoping_rules.rs
@@ -15,6 +15,18 @@ fn main() {
     // `_box2` is destroyed here, and memory gets freed
     let _x = ToDrop;
     println!("Made a ToDrop!");
+
+    // Create an immutable Book named `immutabook`
+    let immutabook = Book {
+        // string literals have type `&'static str`
+        author: "Douglas Hofstadter",
+        title: "Gödel, Escher, Bach",
+        year: 1979,
+    };
+    let mut mutabook = immutabook;
+    borrow_book(&immutabook);
+    borrow_book(&mutabook);
+    new_edition(&mut mutabook);
 }
 
 fn raii() {
@@ -28,5 +40,24 @@ impl Drop for ToDrop {
     fn drop(&mut self) {
         println!("ToDrop is being dropped");
     }
+}
+
+#[derive(Clone, Copy)]
+struct Book {
+    // `&'static str` is a reference to a string allocated in read only memory
+    author: &'static str,
+    title: &'static str,
+    year: u32,
+}
+
+// This function takes a reference to a book
+fn borrow_book(book: &Book) {
+    println!("I immutably borrowed {} - {} edition", book.title, book.year);
+}
+
+// This function takes a reference to a mutable book and changes `year` to 2014
+fn new_edition(book: &mut Book) {
+    book.year = 2014;
+    println!("I mutably borrowed {} - {} edition", book.title, book.year);
 }
 
