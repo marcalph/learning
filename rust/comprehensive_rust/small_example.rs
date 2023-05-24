b8309f37be831b5ea769688e29e772a9375daff1
marcalph <marcalph@protonmail.com>
2023-05-24 10:05:05 +0200

    wip

--- rust/comprehensive_rust/small_example.rs
+++ rust/comprehensive_rust/small_example.rs
@@ -11,3 +11,29 @@ fn main() {              // Program entry point
     }
     println!();
 }
+
+
+// Be sure to note the difference between let mut ref_x: &i32 and let ref_x: &mut i32. 
+// The first one represents a mutable reference which can be bound to different values, 
+// while the second represents a reference to a mutable value.
+
+
+
+// In summary, const values are compile-time constants that are always inlined,
+// while static variables have a fixed memory location and can be mutable. const values are evaluated at compile-time,
+// whereas static variables are lazily initialized at runtime.
+
+
+
+// shadowing let us hold onto valeus after unwrap
+// fn main() {
+//     let a = 1;
+//     let b = &a;
+//     let a = a + 1;
+//     println!("{a} {b}");
+// }
+
+
+
+// &str an immutable reference to a string slice.
+// String a mutable string buffer.
