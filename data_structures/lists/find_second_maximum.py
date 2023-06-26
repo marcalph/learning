9a674fcf418574ce2920d87f79d916496eaa7976
marcalph <marcalph@protonmail.com>
2023-06-26 16:02:31 +0200

    wip

--- data_structures/lists/find_second_maximum.py
+++
@@ -1,4 +0,0 @@
-# oImplement a function find_second_maximum(lst) which returns the second largest element in the list.
-# O(1) space O(n) time
-
-
---
+++ dl/torch101/finetune.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+from PIL import Image
+from tempfile import TemporaryDirectory
+
+cudnn.benchmark = True
+plt.ion()   # interactive mode
